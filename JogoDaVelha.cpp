978baa482f4160e1dfe05e1c61ee3f00269274ec
niltonmng <nilton.ginani@ccc.ufcg.edu.br>
2017-06-07 13:40:39 -0300

    algumas correcoes

--- JogoDaVelha.cpp
+++ JogoDaVelha.cpp
@@ -5,183 +5,172 @@
 
 using namespace std;
 
-string jogador1;
-string jogador2;
 char matriz[3][3];
-string jogada;
-bool turnoPrimeiro = false;
+char jogada[1];
+char jogador1[50];
+char jogador2[50];
+bool isVezPrimeiro = false;
 
-void iniciaJogo(string jogador1, string jogador2);
-void exibeJogo();
-bool isVitoria();
-void jogaX(string posicao);
-void jogaO(string posicao[]);
-void iniciaMatriz();
+void iniciaMatriz(){
+  matriz[0][0] = 'a';
+  matriz[0][1] = 'b';
+  matriz[0][2] = 'c';
 
-int main() {
+  matriz[1][0] = 'd';
+  matriz[1][1] = 'e';
+  matriz[1][2] = 'f';
 
-  iniciaJogo(jogador1, jogador2);
-
-
-  printf("%s\n", "a | b | c");
-  printf("%s\n", "---------");
-  printf("%s\n", "d | e | f");
-  printf("%s\n", "---------");
-  printf("%s\n", "g | h | i");
-
-  iniciaMatriz();
-
-  while(true){
-    if(turnoPrimeiro == true){
-      std::cout << "Vez do Jogador 1."  << "\n";
-      std::cout << "Indique onde quer jogar: " << "\n";
-      scanf("%s", &jogada[0]);
-      jogaX(jogada);
-      turnoPrimeiro = false;
-      // jogada[0] = " ";
-      if(isVitoria()){
-        std::cout << "Jogador 1 ganhou" << "\n";
-      }
-    }
-    else {
-      std::cout << "Vez do Jogador 2."  << "\n";
-      std::cout << "Indique onde quer jogar: " << "\n";
-      scanf("%s", &jogada[0]);
-      jogaO(jogada);
-      turnoPrimeiro = true;
-      // jogada[0] = " ";
-      if(isVitoria()){
-        std::cout << "Jogador 2 ganhou" << "\n";
-      }
-    }
-    exibeJogo();
-
-  }
-
-  exibeJogo();
-
-  return 0;
+  matriz[2][0] = 'g';
+  matriz[2][1] = 'h';
+  matriz[2][2] = 'i';
 }
-
-
+void imprimeMatriz(){
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      std::cout << matriz[i][j] << ' ';
+    }
+    std::cout << '\n';
+  }
+}
 bool isVitoria(){
-  if(((matriz[0][0] == "O") && (matriz[0][1] == "O") && (matriz[0][2] == "O")) || // primeira horizontal
-     ((matriz[0][0] == "X") && (matriz[0][1] == "X") && (matriz[0][2] == "X")) ||
+  if(((matriz[0][0] == 'O') && (matriz[0][1] == 'O') && (matriz[0][2] == 'O')) || // primeira horizontal
+     ((matriz[0][0] == 'X') && (matriz[0][1] == 'X') && (matriz[0][2] == 'X')) ||
 
-     ((matriz[1][0] == "O") && (matriz[1][1] == "O") && (matriz[1][2] == "O")) || // segunda horizontal
-     ((matriz[1][0] == "X") && (matriz[1][1] == "X") && (matriz[1][2] == "X")) ||
+     ((matriz[1][0] == 'O') && (matriz[1][1] == 'O') && (matriz[1][2] == 'O')) || // segunda horizontal
+     ((matriz[1][0] == 'X') && (matriz[1][1] == 'X') && (matriz[1][2] == 'X')) ||
 
-     ((matriz[2][0] == "O") && (matriz[2][1] == "O") && (matriz[2][2] == "O")) || // terceira horizontal
-     ((matriz[2][0] == "X") && (matriz[2][1] == "X") && (matriz[2][2] == "X")) ||
+     ((matriz[2][0] == 'O') && (matriz[2][1] == 'O') && (matriz[2][2] == 'O')) || // terceira horizontal
+     ((matriz[2][0] == 'X') && (matriz[2][1] == 'X') && (matriz[2][2] == 'X')) ||
 
-     ((matriz[0][0] == "O") && (matriz[1][1] == "O") && (matriz[2][2] == "O")) || // diagonal
-     ((matriz[0][0] == "X") && (matriz[1][1] == "X") && (matriz[2][2] == "X")) ||
+     ((matriz[0][0] == 'O') && (matriz[1][1] == 'O') && (matriz[2][2] == 'O')) || // diagonal
+     ((matriz[0][0] == 'X') && (matriz[1][1] == 'X') && (matriz[2][2] == 'X')) ||
 
-     ((matriz[0][0] == "O") && (matriz[1][0] == "O") && (matriz[2][0] == "O")) || // primeira vertical
-     ((matriz[0][0] == "X") && (matriz[1][0] == "X") && (matriz[2][0] == "X")) ||
+     ((matriz[0][0] == 'O') && (matriz[1][0] == 'O') && (matriz[2][0] == 'O')) || // primeira vertical
+     ((matriz[0][0] == 'X') && (matriz[1][0] == 'X') && (matriz[2][0] == 'X')) ||
 
-     ((matriz[0][1] == "O") && (matriz[1][1] == "O") && (matriz[2][1] == "O")) || // segunda vertical
-     ((matriz[0][1] == "X") && (matriz[1][1] == "X") && (matriz[2][1] == "X")) ||
+     ((matriz[0][1] == 'O') && (matriz[1][1] == 'O') && (matriz[2][1] == 'O')) || // segunda vertical
+     ((matriz[0][1] == 'X') && (matriz[1][1] == 'X') && (matriz[2][1] == 'X')) ||
 
-     ((matriz[0][2] == "O") && (matriz[1][2] == "O") && (matriz[2][2] == "O")) || // terceira vertical
-     ((matriz[0][2] == "X") && (matriz[1][2] == "X") && (matriz[2][2] == "X")))
+     ((matriz[0][2] == 'O') && (matriz[1][2] == 'O') && (matriz[2][2] == 'O')) || // terceira vertical
+     ((matriz[0][2] == 'X') && (matriz[1][2] == 'X') && (matriz[2][2] == 'X')))
      {
         return true;
      }
      return false;
 }
+void menuInicio(){
+  std::cout << "Bem vindo ao jogo da Velha!" << '\n';
+  std::cout << "Digite o nome do Jogador 1" << '\n';
+  gets(jogador1);
+  std::cout << "Jogador 1 joga com X" << '\n';
+  std::cout << "Digite o nome do Jogador 2" << '\n';
+  gets(jogador2);
+  std::cout << "Jogador 2 joga com O" << '\n';
 
-void jogaX(string posicao){
-
-  if(posicao == "a"){
-    matriz[0][0] = "X";
-  }
-  if(posicao == "b"){
-    matriz[0][1] = "X";
-  }
-  if(posicao == "c"){
-    matriz[0][2] = "X";
-  }
-  if(posicao == "d"){
-    matriz[1][0] = "X";
-  }
-  if(posicao == "e"){
-    matriz[1][1] = "X";
-  }
-  if(posicao == "f"){
-    matriz[1][2] = "X";
-  }
-  if(posicao == "g"){
-    matriz[2][0] = "X";
-  }
-  if(posicao == "h"){
-    matriz[2][1] = "X";
-  }
-  if(posicao == "i"){
-    matriz[2][2] = "X";
-  }
-
+  std::cout << '\n';
+  std::cout << "Estas sao as Posicoes que estao disponiveis para jogo." << '\n';
+  imprimeMatriz();
 }
 
-void jogaO(string posicao){
-
+void jogadaX(char posicao[]){
   if(posicao == "a"){
-    matriz[0][0] = "O";
+    matriz[0][0] = 'X';
   }
   if(posicao == "b"){
-    matriz[0][1] = "O";
+    matriz[0][1] = 'X';
   }
   if(posicao == "c"){
-    matriz[0][2] = "O";
+    matriz[0][2] = 'X';
   }
   if(posicao == "d"){
-    matriz[1][0] = "O";
+    matriz[1][0] = 'X';
   }
   if(posicao == "e"){
-    matriz[1][1] = "O";
+    matriz[1][1] = 'X';
   }
   if(posicao == "f"){
-    matriz[1][2] = "O";
+    matriz[1][2] = 'X';
   }
   if(posicao == "g"){
-    matriz[2][0] = "O";
+    matriz[2][0] = 'X';
   }
   if(posicao == "h"){
-    matriz[2][1] = "O";
+    matriz[2][1] = 'X';
   }
   if(posicao == "i"){
-    matriz[2][2] = "O";
+    matriz[2][2] = 'X';
   }
-
+}
+void jogadaO(char posicao[]){
+  if(posicao == "a"){
+    matriz[0][0] = 'O';
+  }
+  if(posicao == "b"){
+    matriz[0][1] = 'O';
+  }
+  if(posicao == "c"){
+    matriz[0][2] = 'O';
+  }
+  if(posicao == "d"){
+    matriz[1][0] = 'O';
+  }
+  if(posicao == "e"){
+    matriz[1][1] = 'O';
+  }
+  if(posicao == "f"){
+    matriz[1][2] = 'O';
+  }
+  if(posicao == "g"){
+    matriz[2][0] = 'O';
+  }
+  if(posicao == "h"){
+    matriz[2][1] = 'O';
+  }
+  if(posicao == "i"){
+    matriz[2][2] = 'O';
+  }
 }
 
-void exibeJogo() {
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      printf("%s", &matriz[i][j]);
+int main() {
+  iniciaMatriz();
+  menuInicio();
+
+  // bug no isVitoria, nao retorna se venceu ou nao o jogo
+  // valores de X e O nao estao sendo atribuidos á matriz
+
+  while (true) {
+    if(isVezPrimeiro){
+
+      std::cout << "Jogador 1 escolha uma posicao para jogar." << '\n';
+      gets(jogada);
+      jogadaX(jogada);
+
+      imprimeMatriz();
+      if(isVitoria()){
+        std::cout << "Vitoria Jogador 1 e o Vencedor!" << '\n';
+        break;
+      }
+      if(jogada[0] == 's'){
+        break;
+      }
+      isVezPrimeiro = false;
     }
-    std::cout << "\n";
+    else {
+
+      std::cout << "Jogador 2 escolha uma posicao para jogar." << '\n';
+      gets(jogada);
+      jogadaO(jogada);
+
+      if(isVitoria()){
+        std::cout << "Vitoria Jogador 2 e o Vencedor!" << '\n';
+        break;
+      }
+      if(jogada[0] == 's'){
+        break;
+      }
+      isVezPrimeiro = true;
+    }
   }
-}
 
-void iniciaMatriz(){
-  matriz[0][0] = "a";
-  matriz[0][1] = "b";
-  matriz[0][2] = "c";
-
-  matriz[1][0] = "d";
-  matriz[1][1] = "e";
-  matriz[1][2] = "f";
-
-  matriz[2][0] = "g";
-  matriz[2][1] = "h";
-  matriz[2][2] = "i";
-}
-
-void iniciaJogo(char jogador1[], char jogador2[]){
-  printf("%s\n", "Bem vindo ao jogo da Velha!");
-  printf("%s\n", "Digite o nome do Jogador 1: ");
-  scanf("%s", &jogador1);
-  printf("%s\n", "Digite o nome do Jogador 2: ");
-  scanf("%s", &jogador2);
+  return 0;
 }
